fe3e05a8c358d68026da204936ccddae275f1bcd
Mohammed-Kamil-E3714 <mohammed.kamil@freshworks.com>
2025-05-07 12:11:03 +0530

    Changes

--- src/algorithm/base/Driver.java
+++ src/algorithm/base/Driver.java
@@ -5,8 +5,5 @@ import java.util.*;
 public class Driver {
 
 	public static void main(String[] args) throws InterruptedException {
-		for (char firstChar = 'a'; firstChar <= 'z'; ++firstChar) {
-			System.out.println(firstChar);
-		}
 	}
 }
